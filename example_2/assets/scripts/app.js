fd213891b80c974611a76c4f881c028bd77a4308
ek-so <eksomail@gmail.com>
2025-05-26 09:24:43 +0200

    Section 2. Functions

--- example_2/assets/scripts/app.js
+++ example_2/assets/scripts/app.js
@@ -1,8 +1,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert("The result is: " + result);
+}
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+add(2, 3);
+add(5, 7);
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 - 1";
 
 outputResult(currentResult, calculationDescription);

d349ec4afbaf2f78337a3b65a1b5724f97d014a2
ek-so <eksomail@gmail.com>
2025-05-26 09:06:13 +0200

    Section 2. Assignment 1

---
+++ assignment_1/assignment.js
@@ -0,0 +1,9 @@
+let inputVariable = 30;
+let resultVariable;
+
+resultVariable = inputVariable + 18;
+resultVariable = resultVariable - 6;
+resultVariable = resultVariable * 3;
+resultVariable = resultVariable / 2;
+
+alert(inputVariable + ' and ' + resultVariable);

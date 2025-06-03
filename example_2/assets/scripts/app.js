30834a2357a041cb67b8b9954e7e74277870d7db
ek-so <eksomail@gmail.com>
2025-06-03 09:19:30 +0200

    Section 2. Wrap up

--- example_2/assets/scripts/app.js
+++ example_2/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
+console.log(typeof 1);
 
 //Gets input from input field
 function getUserNumberInput() {
@@ -13,20 +14,28 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   outputResult(currentResult, calcDescription); //from vendor file
 }
 
+function writeToLog (
+  operationIdentifier, 
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
 function add() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult += enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
-  const logEntry = {
-    operation: "ADD",
-    prevResult: initialResult,
-    number: enteredNumber,
-    result: currentResult
-  };
-  logEntries.push(logEntry);
-  console.log(logEntry.operation);
-  console.log(logEntries);
+  writeToLog ("ADD", initialResult, enteredNumber, currentResult);
 }
 
 function subtract() {
@@ -34,6 +43,7 @@ function subtract() {
   const initialResult = currentResult;
   currentResult -= enteredNumber;
   createAndWriteOutput('-', initialResult, enteredNumber);
+  writeToLog ("SUBTRACT", initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
@@ -41,6 +51,7 @@ function multiply() {
   const initialResult = currentResult;
   currentResult *= enteredNumber;
   createAndWriteOutput('*', initialResult, enteredNumber);
+  writeToLog ("MULTIPLY", initialResult, enteredNumber, currentResult);
 }
 
 function divide() {
@@ -48,6 +59,7 @@ function divide() {
   const initialResult = currentResult;
   currentResult /= enteredNumber;
   createAndWriteOutput('/', initialResult, enteredNumber);
+  writeToLog ("SUBSTRACT", initialResult, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener('click', add);
